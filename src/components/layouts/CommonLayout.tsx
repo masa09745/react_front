807fbc512002e3c378dda7ef288ffd22d5e48717
Masa H <masamasa0669@gmail.com>
2022-04-19 22:50:00 +0900

    CommonLayoutエラー修正完了

--- src/components/layouts/CommonLayout.tsx
+++ src/components/layouts/CommonLayout.tsx
@@ -1,32 +1,28 @@
 import React from 'react'
 
 import{ Container, Grid } from "@mui/material"
-import{ makeStyles } from '@mui/material/styles'
+import{ styled } from '@mui/material/styles'
 
 import Header from 'components/layouts/Header'
 
 
-const useStyles = makeStyles(() => ({
-  container: {
-    paddingTop: "3rem"
-  }
-}))
+const CustomContainer = styled(Container)({
+  paddingTop: "3rem"
+})
 
 interface CommonLayoutProps {
   children: React.ReactElement
 }
 
-const CommonLayout =({ children } : CommonLayoutProps) => {
-  const classes = useStyles()
-
+const CommonLayout = ({ children }: CommonLayoutProps) => {
   return (
     <>
       <header>
         <Header />
       </header>
       <main>
-        <Container maxWidth="lg" className={classes.container}>
-          <Grid container justify="center">
+        <Container maxWidth="lg">
+          <Grid>
             <Grid item>
               {children}
             </Grid>

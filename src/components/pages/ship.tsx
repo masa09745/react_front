3e90749943edf11723644d5df83f663bf0279b9f
Masa H <masamasa0669@gmail.com>
2022-06-18 23:56:52 +0900

    Shipページにベタ書きでレイアウト作成

--- src/components/pages/ship.tsx
+++ src/components/pages/ship.tsx
@@ -3,8 +3,8 @@ import React, { useState, useEffect } from 'react';
 import { ShipData } from 'interfaces/index';
 import { ship } from 'lib/api/ship';
 
-import { Box, Card, CardActionArea, CardContent, Typography } from "@mui/material";
-
+import { Box, Card, CardActionArea, CardContent, Typography, Tab, } from "@mui/material";
+import { TabContext, TabPanel, TabList} from '@mui/lab'
 
 import { ShipList } from 'components/utils/ShipList'
 import { ShipDetails } from 'components/pages/ShipDetails'
@@ -13,6 +13,11 @@ import { ShipDetails } from 'components/pages/ShipDetails'
 export const Ship: React.FC = () => {
   const [ships, setShips] = useState<ShipData[]>([])
   const [selectShip, setSelectShip] = useState("")
+  const [value, setValue] = useState("1")
+
+  const handleChange = (event: React.SyntheticEvent, newValue:string) =>{
+    setValue(newValue)
+  };
 
   useEffect (() => {
     const fetchShip = async () => {
@@ -25,7 +30,7 @@ export const Ship: React.FC = () => {
   return(
     <>
       <Box>
-        <Typography sx={{ mb:1}}>
+        <Typography sx={{ mb:1 }}>
           機材一覧
         </Typography>
         <Box
@@ -34,6 +39,7 @@ export const Ship: React.FC = () => {
             flexWrap:" wrap",
             gap: 2,
             mb: 5,
+            px: 2,
           }}
         >
           {ships.map((ship) =>
@@ -57,12 +63,27 @@ export const Ship: React.FC = () => {
           )}
         </Box>
       </Box>
-      <Typography>
+      <Typography sx={{mb:1}}>
         機材情報
       </Typography>
-        <Box>
-          {selectShip}
+        <Box sx={{px:2}}>
+          選択中の機番 : {selectShip}
         </Box>
+        <Box sx={{width: '100%', typography:'body1'}}>
+          <TabContext value={value}>
+            <Box sx={{borderBottom:1, borderColor:'divider'}}>
+              <TabList onChange={handleChange} aria-label="tab API Test">
+                <Tab label="スケジュール" value="1" />
+                <Tab label="整備" value="2" />
+                <Tab label="旅客" value="3" />
+              </TabList>
+            </Box>
+            <TabPanel value="1">スケジュール</TabPanel>
+            <TabPanel value="2">整備情報</TabPanel>
+            <TabPanel value="3">旅客情報</TabPanel>
+          </TabContext>
+        </Box>
+
     </>
   )
 }

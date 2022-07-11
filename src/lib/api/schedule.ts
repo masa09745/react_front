ee445600bdb9ab96a833df7d00755a7f51fabc2b
Masa H <masamasa0669@gmail.com>
2022-07-11 09:36:07 +0900

    shipに対応するscheduleをship側から受け取る

--- src/lib/api/schedule.ts
+++
@@ -1,5 +0,0 @@
-import client from 'lib/api/client';
-
-export const schedule = (id: string | undefined) => {
-  return client.get(`ships/${id}/schedules`)
-}
--- src/lib/api/ship.ts
+++ src/lib/api/ship.ts
@@ -4,3 +4,6 @@ export const ship = () => {
   return client.get('/ships');
 };
 
+export const schedule = (id: string | undefined) => {
+  return client.get(`ships/${id}/schedules`)
+}

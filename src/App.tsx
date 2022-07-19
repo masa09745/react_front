b1e4044accdc379c89c6ac7831a01a48f05c4039
Masa H <masamasa0669@gmail.com>
2022-07-19 18:04:32 +0900

    不要なもの削除、未ログイン時、ログインページへ遷移

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,36 @@
-import React, {useEffect, useState, useContext }  from "react"
+import React, { useContext }  from "react"
 import { BrowserRouter, Routes, Route, Navigate }  from "react-router-dom"
 
 import { CommonLayout } from "components/layouts/CommonLayout"
 import { Home } from "components/pages/Home"
 import { SignIn } from "components/pages/SignIn"
 import { SignUp } from "components/pages/SignUp"
-import { Menu }from "components/pages/Menu"
 import { Schedule } from "components/pages/Schedule"
 import { Ship } from "components/pages/Ship"
 
-import { getCurrentUser } from "lib/api/auth"
-import { User } from "interfaces/index"
 import {AuthContext} from "components/providers/AuthContextProvider"
-
 
 export const App: React.FC = () =>  {
 
-  const contextValue = useContext(AuthContext)
-  console.log(contextValue)
+  const {isSignedIn} = useContext(AuthContext)
+
+  const PrivateRoute = ({ children }: { children: JSX.Element }) => {
+    if (!isSignedIn) {
+      return <Navigate to="/signin" replace/>;
+    }
+
+    return children
+  }
 
   return (
     <BrowserRouter>
       <CommonLayout>
         <Routes>
-          <Route  path="/" element={<Home />} />
           <Route  path="/signup" element={<SignUp />} />
           <Route  path="/signin" element={<SignIn />} />
-          <Route  path="/menu" element={<Menu />} />
-          <Route  path="/schedule" element={<Schedule />} />
-          <Route  path="/ship" element={<Ship />} />
+          <Route  path="/ship" element={ <PrivateRoute><Ship /></PrivateRoute>} />
+          <Route  path="/schedule" element={ <PrivateRoute><Schedule /></PrivateRoute>} />
+          <Route  path="/" element={ <PrivateRoute><Home /></PrivateRoute> }/>
         </Routes>
       </CommonLayout>
     </BrowserRouter>

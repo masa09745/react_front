344d856c3c094a229a17c7bd8234f2de966e1981
Masa H <masamasa0669@gmail.com>
2022-09-16 15:48:18 +0900

    モーダルでの整備情報登録画面完成(CSSは別で)

--- src/components/utils/Modal.tsx
+++ src/components/utils/Modal.tsx
@@ -5,14 +5,24 @@ import Typography from '@mui/material/Typography';
 import TextField from '@mui/material/TextField';
 import Grid from '@mui/material/Grid';
 import Modal from '@mui/material/Modal';
-import TextareaAutosize from '@mui/base/TextareaAutosize';
+import Checkbox from '@mui/material/Checkbox';
+import FormControlLabel  from '@mui/material/FormControlLabel';
+import Select from '@mui/material/Select';
+import MenuItem from '@mui/material/MenuItem';
+import InputLabel from '@mui/material/FormControlLabel';
+import FormControl from '@mui/material/FormControl';
 
 import { useForm, SubmitHandler, Controller } from "react-hook-form"
 
 import {ShipContext} from "components/providers/ShipContextProvider"
+import { AuthContext } from "components/providers/AuthContextProvider"
 
 type Inputs = {
   title: string
+  ATA: string
+  MaintenanceMessage: string
+  Checkbox: boolean
+  Select: number
   description: string
 }
 
@@ -22,11 +32,18 @@ export const BasicModal = () => {
   const handleClose = () => setOpen(false);
 
   const {selectShipId, selectShip} = useContext(ShipContext)
+  const { currentUser } = useContext(AuthContext)
+
+  console.log(currentUser?.employeeNumber)
 
   const { control } = useForm<Inputs>({
     defaultValues: {
       title: "",
-      description: ""
+      ATA: "",
+      MaintenanceMessage: "",
+      Checkbox: true,
+      description: "",
+      Select: 10,
     }
   });
 
@@ -47,7 +64,7 @@ export const BasicModal = () => {
     top: '50%',
     left: '50%',
     transform: 'translate(-50%, -50%)',
-    width: 650,
+    width: 700,
     bgcolor: 'background.paper',
     border: '2px solid #000',
     boxShadow: 24,
@@ -66,29 +83,73 @@ export const BasicModal = () => {
       >
         <Box sx={style}>
           <Typography id="modal-modal-title" variant="h5" component="h1">
-            {selectShip}
+            選択中の機番: {selectShip}
           </Typography>
-          <Box component="form">
+          <Box component="form" id="modal-modal-description">
             <Grid container spacing={2}>
               <Grid item xs={12}>
+                <label>タイトル</label>
                 <Controller
-                  render={({ field }) => <TextField fullWidth  label="タイトル" {...field} />}
+                  render={({ field }) => <TextField fullWidth {...field} />}
                   name="title"
                   control={control}
                 />
               </Grid>
-              <Grid item xs={12}>
+              <Grid item xs={3}>
+                <label>ATA</label>
                 <Controller
-                render={({ field }) => <TextareaAutosize minRows={5}  {...field} />}
+                  render={({ field }) => <TextField fullWidth {...field} />}
+                  name="ATA"
+                  control={control}
+                />
+              </Grid>
+              <Grid item xs={5}>
+                <label>MaintenanceMessage</label>
+                <Controller
+                  render={({ field }) => <TextField fullWidth {...field} />}
+                  name="MaintenanceMessage"
+                  control={control}
+                />
+              </Grid>
+              <Grid item xs={4}>
+                <label>優先度</label>
+                <Controller
+                render={({ field }) => (
+                  <FormControl fullWidth>
+                    <Select {...field}>
+                      <MenuItem value={10}>高</MenuItem>
+                      <MenuItem value={20}>中</MenuItem>
+                      <MenuItem value={30}>低</MenuItem>
+                    </Select>
+                  </FormControl>
+
+                )}
+                name="Select"
+                control={control}
+              />
+              </Grid>
+              <Grid item xs={12}>
+                <label>内容</label>
+                <Controller
+                render={({ field }) => <TextField multiline minRows={5} maxRows={10} fullWidth {...field} />}
                 name="description"
                 control={control}
               />
               </Grid>
+              <Grid item xs={6}>
+                <Controller
+                  name="Checkbox"
+                  control={control}
+                  render={({ field }) =>
+                    <FormControlLabel
+                      control={ <Checkbox {...field} />}
+                      label="完了"
+                     />
+                  }
+                />
+              </Grid>
             </Grid>
             <Button type="submit">送信</Button>
-            <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-
-            </Typography>
           </Box>
         </Box>
       </Modal>

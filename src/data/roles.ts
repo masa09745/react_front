42d6fec9cd4503c159720b9f7596d6ad7f7b26d8
Masa H <masamasa0669@gmail.com>
2022-09-19 02:27:34 +0900

    整備士と運航乗務員以外のdataを削除

--- src/data/roles.ts
+++ src/data/roles.ts
@@ -3,11 +3,5 @@ import type { RoleData } from "types/role"
 export const roles: RoleData[] = [
   {id:"1", name: "機長", sectionName:"運航乗員部"},
   {id:"2", name: "副操縦士", sectionName:"運航乗員部"},
-  {id:"3", name: "チーフ", sectionName:"客室乗員部"},
-  {id:"4", name: "一般", sectionName:"客室乗員部"},
-  {id:"5", name: "整備士", sectionName:"整備部"},
-  {id:"6", name: "スケジューラー", sectionName:"運航管理部"},
-  {id:"7", name: "人事", sectionName:"総務部"},
-  {id:"8", name: "機材管理", sectionName:"品質管理部"},
-
+  {id:"3", name: "整備士", sectionName:"整備部"},
 ]

81ae1e51b4a5a31996a54e565f0a2727cde9f84b
Masa H <masamasa0669@gmail.com>
2022-10-02 10:38:54 +0900

    本番環境で CORSの確認

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -4,6 +4,9 @@ import { AuthContext } from "components/providers/AuthContextProvider"
 export const Home: React.FC = () =>  {
   const { currentUser } = useContext(AuthContext)
 
+  const API_ENDPOINT = process.env.REACT_API_ENDPOINT!;
+  console.log(API_ENDPOINT)
+
   return(
     <>
       <p>社員番号 : {currentUser?.employeeNumber}</p>
